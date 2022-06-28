919f236b7167f16b736390fb1b9c6cdd3cd6f203
Ali Kaner <ali.kaner@hotmail.com>
2022-06-28 03:36:13 +0300

    arrow  basics

--- Assets/Game/Script/ArrowManagment.cs
+++ Assets/Game/Script/ArrowManagment.cs
@@ -1,27 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowManagment : MonoBehaviour
 {
+    public List<GameObject> arrows = new List<GameObject>();
+    public GameObject arrowObject;
+    public Transform parrent;
     public float minX, maxX;
     public LayerMask layerMask;
     public float distance;
-
+    
     void GetRay()
     {
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = Camera.main.transform.position.z;
         Ray ray = Camera.main.ScreenPointToRay(mousePos);
         RaycastHit hit;
-        if (Pysics.Raycast(ray, out hit, 100, layerMask))
+        if (Physics.Raycast(ray, out hit, 100, layerMask))
         {
             Vector3 mouse = hit.point;
             mouse.x = Math.Clamp(mouse.x, minX, maxX);
             distance = mouse.x;
+            Sort();
         }
-
     }
 
+    void MoveObjects(Transform objectTransform, float degree)
+    {
+        Vector3 pos = Vector3.zero;
+        pos.x = Mathf.Cos(degree * Mathf.Deg2Rad);
+        pos.y = Mathf.Sin(degree * Mathf.Deg2Rad);
+        objectTransform.localPosition = pos * distance;
+    }
+    void Sort()
+    {
+        float angle = 1f;
+        float arrowCount = arrows.Count;
+        angle = 360 / arrowCount;
 
+        for (int i = 0; i < arrowCount; i++)
+        {
+            MoveObjects(arrows[i].transform, i * angle);
+            
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            GetRay();
+        }
+    }
 }
+
